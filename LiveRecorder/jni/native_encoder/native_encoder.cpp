707d493d06bd023a01fb9d2d8a3a90f9e522e476
fanyingming <yingmingfan@gmail.com>
2015-08-10 15:54:30 +0800

    add some native_encoder codes.

--- LiveRecorder/jni/native_encoder/native_encoder.cpp
+++ LiveRecorder/jni/native_encoder/native_encoder.cpp
@@ -1,18 +1,48 @@
 #include <jni.h>
 #include "jni_utils.h"
 
+extern "C" {
+#include "../libx264/include/x264.h"
+}
+
 #define LOG_TAG "native_encoder"
+
+static x264_t *h;
 
 int native_encoder_open(JNIEnv *env, jobject thiz, jint width, jint height)
 {
 	LOGI("open encoder \n");
 
+	static x264_param_t param;
+	char *preset = "ultrafast";
+	char *tune = NULL;
+
+	if( x264_param_default_preset( &param, preset, tune ) < 0 )
+		return -1;
+	x264_param_parse( &param, "bitrate", "300" );
+	x264_param_parse( &param, "fps", "30" );
+	param.i_width = width;
+	param.i_height= height;
+
+	h = x264_encoder_open( &param );
+	if (!h)
+		return -1;
+
 	return 0;
 }
 
-int native_encoder_encode(JNIEnv *env, jobject thiz, jbyteArray pixels, jlong pts)
+int native_encoder_encode(JNIEnv *env, jobject thiz, jbyteArray pixels, jbyteArray output, jlong pts)
 {
 	LOGD("encode a frame \n");
+	int i_frame_size = 0;
+	x264_nal_t *nal;
+	int i_nal;
+	x264_picture_t pic_out;
+	x264_picture_t *pic;
+
+	i_frame_size = x264_encoder_encode( h, &nal, &i_nal, pic, &pic_out );
+
+//	output = nal[0].p_payload;
 
 	return 0;
 }
@@ -20,7 +50,8 @@ int native_encoder_encode(JNIEnv *env, jobject thiz, jbyteArray pixels, jlong pt
 int native_encoder_close()
 {
 	LOGI("close encoder \n");
-
+	if( h )
+		x264_encoder_close( h );
 	return 0;
 }
 
