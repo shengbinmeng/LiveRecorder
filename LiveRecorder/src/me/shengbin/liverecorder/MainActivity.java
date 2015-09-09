b338fe05c99607461fd9e28ef17381f181fc4f35
Shengbin Meng <shengbinmeng@gmail.com>
2015-09-09 18:03:14 +0800

    Change default server to a domain name.

--- LiveRecorder/src/me/shengbin/liverecorder/MainActivity.java
+++ LiveRecorder/src/me/shengbin/liverecorder/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 	private EditText mEditOptions = null;
 	private EditText mEditAddress = null;
 	private final static String DEFAULT_OPTIONS = "videoBitrate:500 audioBitrate:20 videoSize:640x480 videoFps:15 videoEncoder:software";
-	private final static String DEFAULT_SERVER = "rtmp://123.56.150.52/origin/";
+	private final static String DEFAULT_SERVER = "rtmp://rtmpserver1.test.strongene.com/origin/";
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
